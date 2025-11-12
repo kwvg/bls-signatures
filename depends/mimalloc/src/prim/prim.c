03bf222cad5370be809adcfd4151737cb29e1fe7
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-11-13 00:06:39 +0530

    patch: swap conditions to evaluate emscripten-specific case earlier
    __wasi__ is a general case while __EMSCRIPTEN__ is a specific case, the
    narrower case should be interpreted before the broader case

--- depends/mimalloc/src/prim/prim.c
+++ depends/mimalloc/src/prim/prim.c
@@ -14,12 +14,12 @@ terms of the MIT license. A copy of the license can be found in the file
 #elif defined(__APPLE__)
 #include "osx/prim.c"      // macOSX (actually defers to mmap in unix/prim.c)
 
+#elif defined(__EMSCRIPTEN__)
+#include "emscripten/prim.c" // emmalloc_*, + pthread support
+
 #elif defined(__wasi__)
 #define MI_USE_SBRK
 #include "wasi/prim.c"     // memory-grow or sbrk (Wasm)
-
-#elif defined(__EMSCRIPTEN__)
-#include "emscripten/prim.c" // emmalloc_*, + pthread support
 
 #else
 #include "unix/prim.c"     // mmap() (Linux, macOSX, BSD, Illumnos, Haiku, DragonFly, etc.)
